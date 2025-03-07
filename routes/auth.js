c030211c7ff430efccf761ca8f3be4ae865973b7
seanygberg <60160094+seanygberg@users.noreply.github.com>
2025-03-07 12:43:50 -0500

    testing backend

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const { login, signup, logout } = require('../middleware/auth');
 const router = express.Router();
 
 // POST route to register a new user
-router.post('/register', signup);
+router.post('/signup', signup);
 
 // POST route to log in
 router.post('/login', login);
