626e5225684f9d8471021451629acabeebd3dd0c
seanygberg <60160094+seanygberg@users.noreply.github.com>
2025-03-07 13:15:27 -0500

    testing backend

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const client = new Client({
   user: 'postgres',
   host: 'db.kzlmwfnxoahkrvcqdiek.supabase.co',
   database: 'postgres',
-  password: 'Marist23',
+  password: 'pDw3zIk9sw9KuPzn',
   port: 5432,
 });
 
@@ -13,3 +13,5 @@ client.connect()
   .catch(err => console.error('Database connection error:', err.stack));
 
 module.exports = client;
+
+//postgresql://postgres:pDw3zIk9sw9KuPzn@db.kzlmwfnxoahkrvcqdiek.supabase.co:5432/postgres
